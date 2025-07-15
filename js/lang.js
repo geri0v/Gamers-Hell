68cb04a3ae43e847103918638d95d21b91e70f1f
geri0v <69841038+geri0v@users.noreply.github.com>
2025-07-15 12:03:09 +0200

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -22,3 +22,7 @@ export function langMenuHTML(current) {
     </button>`
   ).join("");
 }
+export function getWikiLink(name, lang="en") {
+  const sub = lang !== 'en' ? `${lang}.` : '';
+  return `https://${sub}wiki.guildwars2.com/wiki/${encodeURIComponent(name.replace(/ /g, "_"))}`;
+}
