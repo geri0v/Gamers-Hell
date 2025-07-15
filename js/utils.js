2443ef775164f12d94e0764cd6e838d163005dc0
geri0v <69841038+geri0v@users.noreply.github.com>
2025-07-15 12:18:05 +0200

    Delete js/utils.js

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-export function formatPrice(copper) {
-  if (copper == null) return 'N/A';
-  const gold = Math.floor(copper / 10000);
-  const silver = Math.floor((copper % 10000) / 100);
-  const copperRemainder = copper % 100;
-  return `${gold}g ${silver}s ${copperRemainder}c`;
-}
