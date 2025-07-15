7de5efc2d4a5bd041d0db50700e21996ad2946ee
geri0v <69841038+geri0v@users.noreply.github.com>
2025-07-15 12:18:14 +0200

    Delete js/wiki.js

--- js/wiki.js
+++
@@ -1,5 +0,0 @@
-export function generateWikiLink(name, lang = "en") {
-  if (!name) return null;
-  const sub = lang !== "en" ? lang + "." : "";
-  return `https://${sub}wiki.guildwars2.com/wiki/${encodeURIComponent(name.replace(/ /g, "_"))}`;
-}
