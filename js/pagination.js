4e302cf56f5220ba5d88e8255999309d18ce0629
geri0v <69841038+geri0v@users.noreply.github.com>
2025-07-16 09:49:26 +0200

    Update pagination.js

--- js/pagination.js
+++ js/pagination.js
@@ -1,15 +1,10 @@
 /**
  * Simple pagination: returns first N items * page count
- * @param {Array} list - All filtered items
+ * @param {Array} array - All filtered items
  * @param {Number} pageSize - Items per virtual "page"
  * @param {Number} pageNumber - Page count (1-based)
  * @returns {Array} Total slice of items up to page N
  */
-export function paginate(list, pageSize, pageNumber) {
-  return list.slice(0, pageSize * pageNumber);
-}
-// https://geri0v.github.io/Gamers-Hell/js/pagination.js
-
 export function paginate(array, pageSize, pageNumber) {
   return array.slice(0, pageSize * pageNumber);
 }
